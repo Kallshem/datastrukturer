4521c5d274b52f21fab60d5d60b1ac9d8ebf1faa
Kallshem <johan.lenning@telia.com>
2016-03-23 08:36:29 +0100

    The three is working, except delete function

--- exercises/tree.py
+++ exercises/tree.py
@@ -24,27 +24,115 @@ class BinarySearchTree():
 
     def insert(self, key, value=None):
         """Lägg till en nod i trädet."""
-        pass
+        current = self
+        while True:
+            if key > current.key:
+                if current.right == None:
+                    current.right = BinarySearchTree(key, value)
+                    return
+                else:
+                    current = current.right
+            else:
+                if current.left == None:
+                    current.left = BinarySearchTree(key, value)
+                    return
+                else:
+                    current = current.left
+
 
     def lookup(self, key):
         """Sök efter noden med matchande key.
 
         Returnerar matchande noden eller None.
         """
-        pass
+        current = self
+        while True:
+            if key == current.key:
+                return current
+            else:
+                try:
+                    if key > current.key:
+                        current = current.right
+                    else:
+                        current = current.left
+                except:
+                    raise AttributeError
 
     def delete(self, key):
         """Radera noden med matchande key."""
-        pass
+        def replacement(ins):
+            ins = ins.right
+            while ins.left != None:
+                ins = ins.left
+            return ins
+
+        def FindParent(self, key):
+            current = self
+            while True:
+                if key == current.left:
+                    direction = "left"
+                    return current, direction
+
+                elif key == current.right:
+                    direction = "right"
+                    return current, direction
+                else:
+                    if key > current.key:
+                        current = current.right
+                    elif key <= current.key:
+                        current = current.left
+                    else:
+                        raise AttributeError
+                                   
+        replacer = replacement(self)
+        
+        parent, direction = FindParent(self, key)      
+        
+        if direction == "left":
+            mem_right = parent.left.right
+            mem_left = parent.left.left
+            parent.left = replacer.left
+            parent.left.right = mem_right
+            parent.left.left = mem_left
+            replacer.left = None
+            #objektet som låg på replacer.left försvinner?
+        elif direction == "right":
+            mem_right = parent.right.right
+            mem_left = parent.right.left
+            parent.right = replacer.left
+            parent.right.right = mem_right
+            parent.right.left = mem_left
+            replacer.left = None
+        else:
+            print("FML")
 
     def traverse(self):
         """En in-order traversering av trädets noder.
 
         Implementera som en generator.
         """
-        pass
+
+        if not self.left is None:
+            for value in self.left.traverse():
+                yield value
+                #print("left")
+
+        yield self.key
+        #print("key")
+
+        if not self.right is None:
+            for value in self.right.traverse():
+                yield value
+                #print("right")
 
     def __str__(self):
         """Utskrift av trädets alla noder (in-order)."""
         # Använd traverse...
-        pass
+        return ', '.join([str(x) for x in self.traverse()])
+
+def SetupForTest(ins):
+    ints = [150, 50, 140, 40, 145, 160, 155, 157, 153]
+    for i in ints:
+        ins.insert(i)
+    print("accomplished")
+    return ins
--- tests/test_tree.py
+++ tests/test_tree.py
@@ -3,8 +3,35 @@ from exercises.tree import BinarySearchTree
 
 
 class TreeNodeTests(unittest.TestCase):
-    pass
-
+    def test_init(self):
+        pass
 
 class TreeTests(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.ul = BinarySearchTree(100)
+        self.ul.ints = [150, 50, 140, 40, 145, 160, 155, 157, 153, 55, 49, 51, 39, 41]
+        for i in self.ul.ints:
+            self.ul.insert(i)
+
+    def test_traverse(self):
+        ints = '39, 40, 41, 49, 50, 51, 55, 100, 140, 145, 150, 153, 155, 157, 160'
+        self.assertEqual(str(ints), str(self.ul))
+
+    def test_insert(self):
+        self.assertTrue(self.ul.left.key == 50)
+        self.assertTrue(self.ul.right.key == 150)
+        self.assertEqual(self.ul.right.left.key, 140)
+
+    def test_lookup(self):
+        self.assertTrue(self.ul.left.right == self.ul.lookup(55))
+
+    def test_delete(self):
+        self.ul.delete(50)
+        self.ul.ints.remove(50)
+        self.assertEqual(str(self.ul.ints), str(self.ul))
+
+#        100
+#       /   \
+#     98     101
+#       \       \
+#        99      102
